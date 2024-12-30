01792c4721e52752a5073a4ff38e28d2861736b9
mahiboobnadaf <rasool97260@gmail.com>
2024-12-31 01:32:13 +0530

    removed comment

--- redbus/src/app/buses/busData.js
+++ redbus/src/app/buses/busData.js
@@ -1,10 +1,3 @@
-
-// export const busData = [
-//     { id: 1, name: "Express Bus", departure: "10:00 AM", arrival: "2:00 PM", price: "1500" , availability: 15},
-//     { id: 2, name: "Luxury Bus", departure: "11:00 AM", arrival: "3:00 PM", price: "2500", availability: 39 },
-//     { id: 3, name: "Nightline Bus", departure: "9:00 PM", arrival: "1:00 AM", price: "1800", availability: 23 },
-// ];
-
 export const busData = [
     { 
       id: 1, 
