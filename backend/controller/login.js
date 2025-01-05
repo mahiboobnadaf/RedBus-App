ddcc80deb6b75f4fd00b8964e0f82d3e06bf12a3
mahiboobnadaf <rasool97260@gmail.com>
2025-01-06 00:51:44 +0530

    Authentication done

--- backend/controller/login.js
+++ backend/controller/login.js
@@ -4,15 +4,31 @@ const {connection} = require('../config/dbConfig')
 
 let login = (req,res) =>{
     const {mobile_no,password} = req.body;
-    let loginQuery = "SELECT user_name FROM USERS_TABLE WHERE mobile_no = ? AND password = ?"
-    connection.query(loginQuery,[mobile_no,password], (err,result,fields)=>{
-      if(result.length == 0 || err){
-        console.error(err);
-        return res.end("No User Found" )
-      }
-    //   console.log(result[0].user_name)
-    res.send(`Welcome ${result[0].user_name}`)
-    })
+    
+    if (!mobile_no || !password) {
+        return res.send("Mobile number and password are required.");
+    }
+    
+    let loginQuery = "SELECT user_name,mobile_no,password FROM USERS_TABLE WHERE mobile_no = ? "
+    
+    connection.query(loginQuery,[mobile_no], (err,result)=>{
+     if (err) {
+        console.error("Database error:", err);
+        return res.send("DB server error.");
+    }
+
+    if (result.length == 0){
+        return res.send("Mobile Number Not FOund");
+    }
+
+    if(result[0].password == password){
+        console.log("Authenticated")
+        return res.send(`Welcome ${result[0].user_name}`)
+    }
+
+    return res.send("Password Wrong!!!")
+    
+})
 }
 
 module.exports={login}
