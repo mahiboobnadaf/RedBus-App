aee80fc276a0de236dfc79d90a5ffe29a72da7f9
mahiboobnadaf <rasool97260@gmail.com>
2025-01-06 03:44:13 +0530

    Bcrypt is done

--- backend/controller/login.js
+++ backend/controller/login.js
@@ -1,34 +1,44 @@
-const {connection} = require('../config/dbConfig')
+const {connection} = require('../config/dbConfig');
+const bcrypt = require('bcrypt'); 
 
 // console.log(connection + "**#")
 
 let login = (req,res) =>{
-    const {mobile_no,password} = req.body;
+    try{
+        const {mobile_no,password} = req.body;
     
-    if (!mobile_no || !password) {
-        return res.send("Mobile number and password are required.");
-    }
-    
-    let loginQuery = "SELECT user_name,mobile_no,password FROM USERS_TABLE WHERE mobile_no = ? "
-    
-    connection.query(loginQuery,[mobile_no], (err,result)=>{
-     if (err) {
-        console.error("Database error:", err);
-        return res.send("DB server error.");
-    }
+        if (!mobile_no || !password) {
+            return res.send("Mobile number and password are required.");
+        }
+        
+        let loginQuery = "SELECT user_name,mobile_no,password FROM USERS_TABLE WHERE mobile_no = ? "
+        
+        connection.query(loginQuery,[mobile_no], async (err,result)=>{
+        
+            if (err) {
+            console.error("Database error:", err);
+            return res.send("DB server error.");
+        }
 
-    if (result.length == 0){
-        return res.send("Mobile Number Not FOund");
-    }
+        if (result.length == 0){
+            return res.send("Mobile Number Not FOund");
+        }
 
-    if(result[0].password == password){
-        console.log("Authenticated")
-        return res.send(`Welcome ${result[0].user_name}`)
-    }
+        const isPasswordRight = await bcrypt.compare(password,result[0].password);
+        
+        if(isPasswordRight){
+            console.log("Authenticated")
+            return res.send(`Welcome ${result[0].user_name}`)
+        }
 
-    return res.send("Password Wrong!!!")
-    
-})
+        return res.send("Password Wrong!!!")
+        
+        })
+    }
+    catch(err){
+        console.error(err)
+        return res.send("DB Error")
+    }
 }
 
 module.exports={login}
